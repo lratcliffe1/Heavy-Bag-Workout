ef5d404f6b83721f8fd1f63ff88d45d9e7d057de
Liam Ratcliffe <liam.ratcliffe@btinternet.com>
2022-01-04 12:19:48 +0000

    Initial Commit

---
+++ Workout/WorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutApp.swift
+//  Workout
+//
+//  Created by Liam Ratcliffe on 04/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
